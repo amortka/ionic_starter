1fee703e3016736aeebd0cd5ba529052cf4b9a22
Adam Mortka <amortka@pgs-soft.com>
2015-08-11 15:20:49 +0200

    build fixing

---
+++ gulp/serve.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+var del = require('del');
+var util = require('util');
+var browserSync = require('browser-sync').create();
+
+function browserSyncInit(baseDir, files, browser, port) {
+    var routes = null;
+
+    browser = browser === undefined ? 'default' : browser;
+    port = port === undefined ? 6060 : port;
+
+    browserSync.init({
+        startPath: '/',
+        server: {
+            baseDir: baseDir
+        },
+        files: files,
+        port: port,
+        browser: browser
+    });
+}
+
+gulp.task('serve', ['watch'], function() {
+  browserSyncInit([
+      '.tmp',
+      'app'
+  ], [
+      '.tmp/**/*.css',
+      'app/common/**/*.html',
+      'app/common/**/*.js',
+      'app/components/**/*.html',
+      'app/components/**/*.js',
+      'app/pages/**/*.html',
+      'app/pages/**/*.js'
+  ]);
+
+});
