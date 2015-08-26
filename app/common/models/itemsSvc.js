bb28274df9f4c9b8b9736dd80afe5327a4b97800
Adam Mortka <amortka@pgs-soft.com>
2015-08-26 14:03:49 +0200

    fixing

--- app/common/models/itemsSvc.js
+++ app/common/models/itemsSvc.js
@@ -2,22 +2,18 @@
 angular.module('app.services', [])
     .factory('Item', function ($http) {
 
-        var items = [{
-            name: 'banana',
-            qty: 1
-        }, {
-            name: 'apple',
-            qty: 2
-        }, {
-            name: 'potato',
-            qty: 3
-        }];
+        var items = [
+            {name: 'banana', qty: 1},
+            {name: 'apple', qty: 2},
+            {name: 'potato', qty: 3},
+            {name: 'eggs', qty: 12}
+        ];
 
         var getItems = function () {
             return items;
         };
 
-        var removeItem = function(item) {
+        var removeItem = function (item) {
             _.remove(items, item);
         };
 
